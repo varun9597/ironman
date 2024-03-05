4ed706144b21a6946d1516590d7fd2ba68ca041f
Varun Panditpautra <panditpautrav@gmail.com>
2024-03-05 13:33:21 +0530

    Added add flat functionality

--- app.py
+++ app.py
@@ -73,5 +73,91 @@ def dashboard():
         #flash('Please sign in to access the homepage', 'error')
         return redirect(url_for('sign_in'))
 
+@app.route("/addflat", methods=['GET', 'POST'])
+def addflat():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            print(request.form)
+            cust_name = request.form['customerName']
+            print(cust_name)
+            cust_flat_no = request.form['flatNumber']
+            print(cust_flat_no)
+            cust_soc_name = request.form['society']
+            print(cust_soc_name)
+            cust_phn_no = request.form['customerPhoneNumber']
+            print(cust_phn_no)
+            # Check if the society exists in tbl_society
+            soc_id = None
+
+            try:
+                conn = get_connection()
+                soc_query = text(f"SELECT pk_soc_id FROM tbl_society WHERE soc_name = '{cust_soc_name}';")
+                result = conn.execute(soc_query).fetchone()
+                print(str(result)+" fetching soc_id")
+                if result:
+                    soc_id = list(result)[0]
+            except Exception as e:
+                print(e)
+                flash("Error fetching society data","error")
+                return redirect(url_for("addflat"))
+
+            if soc_id is None:
+                # If the society doesn't exist, add a new entry in tbl_society
+                try:
+                    conn = get_connection()
+                    add_soc_query = text(f"INSERT INTO tbl_society (soc_name) VALUES ('{cust_soc_name}');")
+                    result=conn.execute(add_soc_query)
+                    print(str(result)+ " insert soc details")
+                    soc_id = result.lastrowid
+                    print(str(soc_id)+" soc_id")
+                except Exception as e:
+                    print(e)
+                    flash("Error adding new society","error")
+                    return redirect(url_for("addflat"))
+                
+                print(soc_id)
+
+            #add a new entry in tbl_customer
+            add_cust_query = text(f"INSERT INTO tbl_customer (cust_name, fk_soc_id, cust_phone, fk_user_id) VALUES ('{cust_name}','{soc_id}','{cust_phn_no}','{user_id}');")
+            cust_id = None
+            try:
+                conn = get_connection()
+                result=conn.execute(add_cust_query)
+                print(str(result)+ " insert cust details")
+                cust_id = result.lastrowid
+                print(str(cust_id)+" cust_id")
+            except Exception as e:
+                print(e)
+                flash("Error adding nwe customer","error")
+                return redirect(url_for("addflat"))
+            
+            add_flat_query = text(f"INSERT INTO tbl_cust_flat (flat_no, fk_cust_id, fk_soc_id, fk_user_id) VALUES ('{cust_flat_no}','{cust_id}','{soc_id}','{user_id}');")
+            try:
+                conn = get_connection()
+                conn.execute(add_flat_query)
+            except Exception as e:
+                print(e)
+                flash("Error adding new flat","error")
+                return redirect(url_for("addflat"))
+            flash("Flat added successfully", "success")
+            return redirect(url_for("addflat"))
+        
+        #fetch society list
+        soc_list_query = text("SELECT soc_name from tbl_society;")
+        try:
+            conn = get_connection()
+            societies = [row[0] for row in conn.execute(soc_list_query).fetchall()]
+            print(societies)
+        except Exception as e:
+            print(e)
+            flash("Error fetching soc list","error")
+            societies = []
+        
+        return render_template('add_flat.html',societies=societies)
+
+    else:
+        #flash('Please sign in to access the homepage', 'error')
+        return redirect(url_for('sign_in'))
 if __name__ == "__main__":
     app.run(debug=True)
