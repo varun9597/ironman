20f0f24a62f0e520c9c88f335489ac2c3d567a8d
Varun Panditpautra <panditpautrav@gmail.com>
2024-03-06 00:52:38 +0530

    Added add society feature, navbar to all pages, flash messages

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def sign_in():
         password = request.form['password']
         try:
             conn = get_connection()
-            query = text(f"SELECT id FROM users WHERE username = '{username}' AND password = '{password}'")
+            query = text(f"SELECT id FROM tbl_users WHERE username = '{username}' AND password = '{password}'")
             result = list(conn.execute(query).fetchone())
             print(result)
 
@@ -41,7 +41,7 @@ def sign_up():
 
         try:
             conn = get_connection()
-            query = text(f"INSERT INTO users (name, username, password) VALUES ('{name}', '{username}', '{password}')")
+            query = text(f"INSERT INTO tbl_users (name, username, password) VALUES ('{name}', '{username}', '{password}')")
             conn.execute(query)
             flash('Account created successfully. Please sign in.', 'success')
             return redirect(url_for('sign_in'))
@@ -60,21 +60,27 @@ def logout():
 
 @app.route("/dashboard")
 def dashboard():
-    # Add authentication check
-    if 'user_id' in session:
-        user_id = session['user_id']
-        conn = get_connection()
-        query = text(f"select name from users where id = '{user_id}';")
-        result = list(conn.execute(query).fetchone())
-        if result:
-            name = result[0]
-            return render_template('dashboard.html', username = name)
+    user = fetch_user_name()
+    if user:
+        return render_template('dashboard.html', username = user)
     else:
         #flash('Please sign in to access the homepage', 'error')
         return redirect(url_for('sign_in'))
 
+def fetch_user_name():
+    name = None
+    if 'user_id' in session:
+        user_id = session['user_id']
+        conn = get_connection()
+        query = text(f"select name from tbl_users where id = '{user_id}';")
+        result = list(conn.execute(query).fetchone())
+        if result:
+            name = result[0]
+        return name
+
 @app.route("/addflat", methods=['GET', 'POST'])
 def addflat():
+    user = fetch_user_name()
     if 'user_id' in session:
         user_id = session['user_id']
         if request.method == 'POST':
@@ -92,32 +98,14 @@ def addflat():
 
             try:
                 conn = get_connection()
-                soc_query = text(f"SELECT pk_soc_id FROM tbl_society WHERE soc_name = '{cust_soc_name}';")
+                soc_query = text(f"SELECT pk_soc_id FROM tbl_society WHERE LOWER(soc_name) = LOWER('{cust_soc_name}') and fk_user_id = '{user_id}';")
                 result = conn.execute(soc_query).fetchone()
                 print(str(result)+" fetching soc_id")
                 if result:
                     soc_id = list(result)[0]
             except Exception as e:
                 print(e)
-                flash("Error fetching society data","error")
                 return redirect(url_for("addflat"))
-
-            if soc_id is None:
-                # If the society doesn't exist, add a new entry in tbl_society
-                try:
-                    conn = get_connection()
-                    add_soc_query = text(f"INSERT INTO tbl_society (soc_name) VALUES ('{cust_soc_name}');")
-                    result=conn.execute(add_soc_query)
-                    print(str(result)+ " insert soc details")
-                    soc_id = result.lastrowid
-                    print(str(soc_id)+" soc_id")
-                except Exception as e:
-                    print(e)
-                    flash("Error adding new society","error")
-                    return redirect(url_for("addflat"))
-                
-                print(soc_id)
-
             #add a new entry in tbl_customer
             add_cust_query = text(f"INSERT INTO tbl_customer (cust_name, fk_soc_id, cust_phone, fk_user_id) VALUES ('{cust_name}','{soc_id}','{cust_phn_no}','{user_id}');")
             cust_id = None
@@ -129,7 +117,6 @@ def addflat():
                 print(str(cust_id)+" cust_id")
             except Exception as e:
                 print(e)
-                flash("Error adding nwe customer","error")
                 return redirect(url_for("addflat"))
             
             add_flat_query = text(f"INSERT INTO tbl_cust_flat (flat_no, fk_cust_id, fk_soc_id, fk_user_id) VALUES ('{cust_flat_no}','{cust_id}','{soc_id}','{user_id}');")
@@ -138,26 +125,69 @@ def addflat():
                 conn.execute(add_flat_query)
             except Exception as e:
                 print(e)
-                flash("Error adding new flat","error")
                 return redirect(url_for("addflat"))
-            flash("Flat added successfully", "success")
             return redirect(url_for("addflat"))
         
         #fetch society list
-        soc_list_query = text("SELECT soc_name from tbl_society;")
+        soc_list_query = text(f"SELECT soc_name from tbl_society where fk_user_id = '{user_id}';")
         try:
             conn = get_connection()
             societies = [row[0] for row in conn.execute(soc_list_query).fetchall()]
             print(societies)
         except Exception as e:
             print(e)
-            flash("Error fetching soc list","error")
             societies = []
         
-        return render_template('add_flat.html',societies=societies)
+        return render_template('add_flat.html',societies=societies,username = user)
 
     else:
         #flash('Please sign in to access the homepage', 'error')
         return redirect(url_for('sign_in'))
+
+@app.route("/addsociety", methods = ['GET','POST'])
+def addsociety():
+    user = fetch_user_name()
+    if 'user_id' in session:
+        user_id = session['user_id']
+        if request.method == 'POST':
+            cust_soc_name = request.form['society']
+            print(cust_soc_name)
+            soc_id = None
+
+            try:
+                conn = get_connection()
+                soc_query = text(f"SELECT pk_soc_id FROM tbl_society WHERE LOWER(soc_name) = LOWER('{cust_soc_name}') and fk_user_id = '{user_id}';")
+                result = conn.execute(soc_query).fetchone()
+                print(str(result)+" fetching soc_id")
+                if result:
+                    soc_id = list(result)[0]
+                    flash("Society Name already exists.","error")
+                    return redirect(url_for("addsociety"))
+            except Exception as e:
+                print(e)
+                return redirect(url_for("addsociety"))
+
+            if soc_id is None:
+                # If the society doesn't exist, add a new entry in tbl_society
+                try:
+                    conn = get_connection()
+                    add_soc_query = text(f"INSERT INTO tbl_society (soc_name,fk_user_id) VALUES ('{cust_soc_name}','{user_id}');")
+                    result=conn.execute(add_soc_query)
+                    print(str(result)+ " insert soc details")
+                    soc_id = result.lastrowid
+                    print(str(soc_id)+" soc_id")
+                    flash("New society added.","success")
+                    return redirect(url_for("addsociety"))
+                except Exception as e:
+                    print(e)
+                    return redirect(url_for("addflat"))
+        return render_template('add_society.html',username = user)
+    else:
+        #flash('Please sign in to access the homepage', 'error')
+        return redirect(url_for('sign_in'))
+
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
